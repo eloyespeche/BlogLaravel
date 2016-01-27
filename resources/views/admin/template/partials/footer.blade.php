c9351b958a875c766e76e8174e3d6dbbe71322a1
Eloy <eloyespeche@gmail.com>
2016-01-27 19:02:02 -0300

    Modificada la fecha de la pagina

--- resources/views/admin/template/partials/footer.blade.php
+++ resources/views/admin/template/partials/footer.blade.php
@@ -3,7 +3,7 @@
 		<nav class="navbar navbar-inverse">
 			<div class="container-fluid">
 				<div class="collapse navbar-collapse">
-					<p class="navbar-text">Todos los derechos reservados &copy 2016</p>
+					<p class="navbar-text">Todos los derechos reservados &copy {{ date('Y') }}</p>
 					<p class="navbar-text navbar-right">Eloy Espeche</p>
 				</div>
 			</div>
